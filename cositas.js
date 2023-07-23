312e24c53b330152e852bc3d521272467355d3c6
TheChack <augustochacana60@hotmail.com>
2023-07-23 20:57:52 -0300

    new file:   Assets/Icons/config.svg 	new file:   Assets/Icons/darkmode-luna.svg 	new file:   Assets/Icons/darkmode-sol.svg 	new file:   Assets/Icons/email.svg 	new file:   Assets/Icons/github.svg 	new file:   Assets/Icons/linkedin.svg 	new file:   Assets/Icons/whatsapp.svg 	new file:   Assets/Icons/wordpress.svg 	deleted:    Assets/Media/Banner-bg.jpg 	new file:   Assets/Media/Banner-bg1.jpg 	new file:   Assets/Media/Banner-bg2.jpg 	modified:   Index.html 	modified:   cositas.js 	modified:   estilo.css

--- cositas.js
+++ cositas.js
@@ -1,10 +1,48 @@
 function checkboxAdj() {
-    const checkbox = document.getElementById('adjuntar-archivo');
-    const boton = document.getElementById('subir-archivos');
+  const checkbox = document.getElementById('adjuntar-archivo');
+  const boton = document.getElementById('subir-archivos');
 
-    if (checkbox.checked == true){
-        boton.style.display = "block";
-      } else {
-        boton.style.display = "none";
+  if (checkbox.checked == true){
+      boton.style.display = "block";
+    } else {
+      boton.style.display = "none";
+    }
+}
+
+function menuConfig() {
+  var lista = document.getElementById('config-menu');
+
+  lista.classList.toggle("config-mostrar");
+  
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.config-button')) {
+      var dropdowns = document.getElementsByClassName("config-dropdown");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('config-mostrar')) {
+          openDropdown.classList.remove('config-mostrar');
+        }
       }
+    }
+  } 
+
+}
+
+
+function modoOscuro() {
+  var darkBody = document.body;
+  var luna = document.getElementById('dark-mode-img1');
+  var sol = document.getElementById('dark-mode-img2');
+
+  darkBody.classList.toggle('modo-oscuro');
+
+  if (darkBody.className == 'modo-oscuro') {
+    luna.style.display = "none";
+    sol.style.display = "block";
+  } else {
+    luna.style.display = "block";
+    sol.style.display = "none";
+  }
 }
