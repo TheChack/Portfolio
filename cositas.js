11633ea5b3740ec18fd40a94e3db91b043a8d676
TheChack <augustochacana69@hotmail.com>
2023-07-24 12:47:50 -0300

    new file:   Assets/Icons/star-half-stroke-regular.svg 	new file:   Assets/Icons/star-regular.svg 	new file:   Assets/Icons/star-solid.svg 	modified:   Index.html 	modified:   cositas.js 	modified:   estilo.css

--- cositas.js
+++ cositas.js
@@ -14,7 +14,6 @@ function menuConfig() {
 
   lista.classList.toggle("config-mostrar");
   
-
   window.onclick = function(event) {
     if (!event.target.matches('.config-button')) {
       var dropdowns = document.getElementsByClassName("config-dropdown");
@@ -35,14 +34,36 @@ function modoOscuro() {
   var darkBody = document.body;
   var luna = document.getElementById('dark-mode-img1');
   var sol = document.getElementById('dark-mode-img2');
+  var banner = document.getElementById('header-banner');
 
   darkBody.classList.toggle('modo-oscuro');
 
   if (darkBody.className == 'modo-oscuro') {
     luna.style.display = "none";
     sol.style.display = "block";
+    banner.style.backgroundImage = "url(./Assets/Media/Banner-bg2.jpg)";
   } else {
     luna.style.display = "block";
     sol.style.display = "none";
+    banner.style.backgroundImage = "url(./Assets/Media/Banner-bg1.jpg)";
   }
 }
+
+function fechaHora() {
+  var span = document.getElementById('hora');
+
+  var d = new Date();
+  var s = d.getSeconds();
+  var m = d.getMinutes();
+  var h = d.getHours();
+  var dia = d.getDay();
+  var diaMes = d.getDate();
+  var mes = d.getMonth();
+
+  var semana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+  var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
+              "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+  span.textContent = h + ":" + m + ":" + s + " | " + semana[dia] + " " + diaMes + " de " + meses[mes];
+
+}
+setInterval(fechaHora, 1000);
